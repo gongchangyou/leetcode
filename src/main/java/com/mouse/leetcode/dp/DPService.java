1851a4bdb93076f5109dda3463000741f020fec2
gongchangyou <gongchangyou@58.com>
2021-12-17 18:38:59 +0800

    dp triangle

---
+++ src/main/java/com/mouse/leetcode/dp/DPService.java
@@ -0,0 +1,93 @@
+package com.mouse.leetcode.dp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author gongchangyou
+ * @version 1.0
+ * @date 2021/12/16 11:23 下午
+ */
+@Slf4j
+@Service
+public class DPService {
+
+
+    /**
+     * 时间复杂度：O(n^2)
+     *
+     * 空间复杂度：O(n^2)
+     *
+     * @param triangle
+     * @return
+     */
+    //https://leetcode-cn.com/problems/IlPe0q/
+    //剑指 Offer II 100. 三角形中最小路径之和
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] f = new int[n][n]; //到达f[i][j]的最短路径 i行j列
+        f[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            f[i][0] = f[i - 1][0] + triangle.get(i).get(0); //当我们在第 i 行的最左侧时，我们只能从第 i−1 行的最左侧移动过来。
+            for (int j = 1; j < i; ++j) {
+                f[i][j] = Math.min(f[i - 1][j - 1], f[i - 1][j]) + triangle.get(i).get(j);
+            }
+            f[i][i] = f[i - 1][i - 1] + triangle.get(i).get(i); //当我们在第 i 行的最右侧时，我们只能从第 i−1 行的最右侧移动过来。
+        }
+        log.info("f={}", f);
+        int minTotal = f[n - 1][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[n - 1][i]);
+        }
+        return minTotal;
+    }
+
+    public int minimumTotalON2(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] f = new int[2][n];
+        f[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            int curr = i % 2;
+            int prev = 1 - curr;
+            f[curr][0] = f[prev][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; ++j) {
+                f[curr][j] = Math.min(f[prev][j - 1], f[prev][j]) + triangle.get(i).get(j);
+            }
+            f[curr][i] = f[prev][i - 1] + triangle.get(i).get(i);
+        }
+        log.info("f={}", f);
+        int minTotal = f[(n - 1) % 2][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[(n - 1) % 2][i]);
+        }
+        return minTotal;
+    }
+
+    /**
+     * 复杂度O(n)
+     * @param triangle
+     * @return
+     */
+    public int minimumTotalON(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] f = new int[n];
+        f[0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            f[0] = f[0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; ++j) {
+                f[j] = Math.min(f[prev][j - 1], f[prev][j]) + triangle.get(i).get(j);
+            }
+            f[i] = f[i - 1] + triangle.get(i).get(i);
+        }
+        log.info("f={}", f);
+        int minTotal = f[(n - 1) % 2][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, f[(n - 1) % 2][i]);
+        }
+        return minTotal;
+    }
+
+
+}
--- src/test/java/com/mouse/leetcode/LeetcodeApplicationTests.java
+++ src/test/java/com/mouse/leetcode/LeetcodeApplicationTests.java
@@ -1,14 +1,50 @@
 package com.mouse.leetcode;
 
+import com.mouse.leetcode.dp.DPService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @SpringBootTest
 class LeetcodeApplicationTests {
 
+	@Resource
+	com.mouse.leetcode.dp.DPService dPService;
+
 	@Test
-	void contextLoads() {
-		System.out.println("yyy");
+	void minimumTotal() {
+		ArrayList params = new ArrayList<List<Integer>>(){
+			{
+				add(new ArrayList<Integer>() {{
+					add(2);
+				}});
+				add(new ArrayList<Integer>() {{
+					add(3);
+					add(4);
+				}});
+				add(new ArrayList<Integer>() {{
+					add(6);
+					add(5);
+					add(7);
+				}});
+				add(new ArrayList<Integer>() {{
+						add(4);
+						add(1);
+						add(8);
+						add(3);
+					}}
+				);
+			}};
+		int r = dPService.minimumTotal(params);
+		log.info("r={}", r);
+
+		int r2 = dPService.minimumTotalON2(params);
+		log.info("r2={}", r2);
 	}
 
 }
